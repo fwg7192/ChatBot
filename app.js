61d297416511f406bbab32280c957b7fc9774fb0
nelz16 <nelzpetersen@gmail.com>
2018-05-24 17:29:07 +1200

    test

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
-  
+  // test chat
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
